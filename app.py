1489b345ca77c12f33443d39d5b0af9308f32477
KatonaMihaly <katona0607@gmail.com>
2025-07-01 10:23:01 +0200

    Dockerization

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ if __name__ == "__main__":
     in debug mode for development purposes.
     """
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
